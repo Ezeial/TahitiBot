529b7bdaf47474efe3c994a5f3dd172560119144
Ezio Giraldi <eziogiraldi.dev@gmail.com>
2020-11-15 22:10:34 +0100

    up

--- events/bindRoleEvent.js
+++ events/bindRoleEvent.js
@@ -3,7 +3,6 @@ const bot = require('../bot')
 const ReactionHandler = require('../class/ReactionHandler')
 
 const handleAddReaction = (guildID, emoji, member) => {
-    console.log('add')
     const roles = bot.guilds.find(guild => guild.id === guildID).roles
     if (!roles.find(role => role.name === emoji.name)) return
     const id = roles.find(role => role.name === emoji.name).id
@@ -21,9 +20,7 @@ module.exports = {
     name: "bindRoleEvent",
     execute: async () => {
         const message = await bot.getMessage(config.menuInfo.channelID, config.menuInfo.messageID)
-
-        console.log(message)
-
+        
         const reactionListener = new ReactionHandler(message)
 
         reactionListener.on('add', handleAddReaction)
